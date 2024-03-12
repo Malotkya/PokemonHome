0b99e639b338af18ab271aa4cb287bbe7cf776f4
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-12 10:13:25 -0500

    main file

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import exportToExcel from "./ExportExcel";
+import filterList from "./FilterList";
+import findPokemon from "./FindPokemon";
+import {downloadHome} from "./Pokemon/Home";
+import fs from "fs";
+
+switch(process.argv[2]){
+    case "export":
+        exportToExcel(process.argv[4])
+            .then(buffer=>{
+                const target:string = process.argv[3] || "export.xlsx";
+                fs.writeFileSync(target, buffer);
+            }).catch(console.error);
+    break;
+
+    case "filter":
+        filterList(process.argv[3])
+            .then(buffer=>{
+                const target:string = process.argv[4] || "filter.xlsx";
+                fs.writeFileSync(target, buffer)
+            }).catch(console.error);
+            
+    break;
+
+    case "find":
+        findPokemon(process.argv[3])
+            .then(results=>{
+                if(results.length === 0) {
+                    console.log("No results found!");
+                } else {
+                    for(const pos of results)
+                        console.log(pos);
+                }
+            }).catch(console.error);
+    break;
+
+    case "update":
+        downloadHome()
+            .then(()=>console.log("Update Complete!"))
+            .catch(console.error);
+    break;
+
+    default:
+        console.error("Unknown process!");
+}
